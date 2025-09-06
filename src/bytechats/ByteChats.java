7f3051b9c2a83e5c0ca4589e6f72d4686e97084d
Asus <Asus@Aru>
2025-09-06 21:50:32 +0530

    Initial Commit

---
+++ src/bytechats/ByteChats.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bytechats;
+
+/**
+ *
+ * @author Asus
+ */
+public class ByteChats {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main.main(args);
+    }
+    
+}
